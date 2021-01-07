dafcf78cf2ba20f9fd2611f176d902893cde822a
John Paul Manoza <johnpaul@bigdish.com>
2021-01-07 16:39:53 +0800

    feat(root): setup root view

--- CleanProject/AppDelegate/AppDelegate.swift
+++ CleanProject/AppDelegate/AppDelegate.swift
@@ -6,11 +6,20 @@
 //
 
 import UIKit
+import SwinjectStoryboard
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    static var shared: AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
+    
+    func initialViewController() -> UIViewController {
+        let sb = SwinjectStoryboard.create(name: "Main", bundle: nil)
+        let vc = sb.instantiateInitialViewController()!
+        return vc
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
